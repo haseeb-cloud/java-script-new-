21abc20a4e6ecc679ccba24abae21b9ff7201878
haseeb-cloud <144657162+haseeb-cloud@users.noreply.github.com>
2025-06-03 09:22:21 +0000

    operation

---
+++ 01_basics/3_conversionOperation2.js
@@ -0,0 +1,45 @@
+// *********** operations *****************
+
+let value =3
+let negValue = -value
+console.log(negValue);
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);  // yah power hai 3 ki power 3
+console.log(2/3);
+console.log(2%2);
+
+
+let str1 = "heelo"
+let str2 = " haseeb"
+
+let str3 = str1 + str2;
+console.log(str3);
+
+
+// yah thora complex hai 
+console.log("1" + 2 );
+console.log(1 + "2");
+console.log( "1" + 2 + 2); // yah thora ajeeb hai 
+console.log(1 + 2 + "7"); // jab yah conbersion ho to issme dependent mat rahna 
+
+
+console.log( 3 + 4 * 5 % 3) ; // reallife mai kahi yah appreciate nahi karega aur likhna hai to bhi parenthesis ka used karo
+
+console.log( true);
+console.log( +true); // yah mostly used nahi hota hai 
+console.log( +"");
+
+
+let num1, num2 , num3;
+
+num1 = num2 = num3 = 2 + 2 // yah sab cheejai bari company mai nahi used hota hai 
+let gamecounter = 100
+gamecounter++; //postfix
+++gamecounter; //prefix
+console.log(gamecounter);
+
+//prefix andpostfix mdn sai jaa kar pardo 
