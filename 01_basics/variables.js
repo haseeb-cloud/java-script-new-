4b184b7d67692697577d89c36b840e553a836f2c
haseeb-cloud <144657162+haseeb-cloud@users.noreply.github.com>
2025-06-03 08:43:41 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,22 @@
+const accountId = 261
+let accountEmail = "haseeb@gmail.com"
+var accountPassword = "1234"
+accountCity = "Jaipur"
+
+let accountState ;
+
+// accountId = 2 
+
+accountEmail = "hc@.com"
+accountPassword = "23220"
+
+
+/*
+we did not prefer to use the var
+because of issue in block scope and fucntional scope
+
+*/
+
+console.log(accountId);
+
+console.table([ accountId, accountEmail, accountPassword , accountCity, accountState])
