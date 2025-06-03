8d46524961d498d38089db8b6df40373a48d9183
haseeb-cloud <144657162+haseeb-cloud@users.noreply.github.com>
2025-06-03 08:43:28 +0000

    datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as the new code
+
+// alert("hello");
+// alert( 3 + 3);
+// we are using nodejs, not browser
+
+
+//dekho alert likhnai mai error aaihai kiyuki yah nodebook mai hai lakin ab ham used nahi karte 
+
+
+console.log(3 + 
+          3);  console.log("haseeb")
+// code readibility must be high 
+
+
+let name = "hitest" // string 
+let age = 18 // number
+let isLoggedIn = false // boolean 
+
+//number => t to power 52
+// bigint   // jab tumhara number bahut bara hai 
+// stirng => ""
+// boolean   => true/false
+// null => standalone value ( null matlab empty)
+//underfined => variable declare kiya hai lakin value define nahi kari 
+// symbol => iska ham mostly used react ami kare gai ( jab bhi hame baat karnti ha unique ki )
+
+
+
+
+//object 
+
+console.log(typeof "haseeb")
+console.log(typeof age)
+console.log(typeof null)  // null aak object hai yaad rakhna 
+console.log(typeof undefined)
