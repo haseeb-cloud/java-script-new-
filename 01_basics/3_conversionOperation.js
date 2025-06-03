94a011bbbd856d20832aa0d07343a412cf8301a6
haseeb-cloud <144657162+haseeb-cloud@users.noreply.github.com>
2025-06-03 09:04:42 +0000

    conversionof datatypes

---
+++ 01_basics/3_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = undefined
+console.log(typeof score);
+console.log(typeof (score));
+
+
+let valueInNumber = Number(score) // yah garentee hai ki jo score hai vo 100% number hai 
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// jab convert nahi hoo pata hai to hamne NaN bhi dekha hai aur 0 bhi dekha hai
+
+// "33" => 33
+//"33abs" => NaN (not a number)
+// true => 1 ; false => 0
+
+let isloggedIn = "haseeb"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true 
+//0 => false 
+// "haseeb" => true 
+
+let someNumber = 33
+ 
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber );
+
+// parhai investivation study karo 
+
+
+
